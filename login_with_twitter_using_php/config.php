b203779a79c615a4b4b00f53fb02ead6ceacafbc
nisargmodi <nisarg92modi@gmail.com>
2016-04-11 23:54:20 -0700

    login module

---
+++ login_with_twitter_using_php/config.php
@@ -0,0 +1,5 @@
+<?php
+define("CONSUMER_KEY", "7uBwsMCamrmNAUqB77qUiGjHc");
+define("CONSUMER_SECRET", "PclZ8z3lrRBuEnACrzUo6tGQulyp7wVH6tj5IRUsS5gG6zWQ9A");
+define('OAUTH_CALLBACK', 'http://localhost/login_with_twitter_using_php/process.php');
+?>
