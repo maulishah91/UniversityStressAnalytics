a5027f1148a6de8996a8284ffdf7f42d29ec6dd1
nisargmodi <nisarg92modi@gmail.com>
2016-05-05 15:59:46 -0700

    changing path

--- login/includes/consumer_keys.php
+++
@@ -1,6 +0,0 @@
-<?php
-define(CONSUMER_KEY, "7uBwsMCamrmNAUqB77qUiGjHc");
-define(CONSUMER_SECRET, "PclZ8z3lrRBuEnACrzUo6tGQulyp7wVH6tj5IRUsS5gG6zWQ9A");
-define(LOCALHOSt, "http://107.170.59.110:8080/stressAnalytics");
-define(LOGIN, "http://107.170.59.110:8080/stressAnalytics/login");
-?>
