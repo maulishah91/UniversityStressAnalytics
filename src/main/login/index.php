52152997b48412d147a176abc35d2fade95962d4
nisargmodi <nisarg92modi@gmail.com>
2016-05-06 16:09:10 -0700

    styling and stuff

--- src/main/login/index.php
+++ src/main/login/index.php
@@ -13,7 +13,35 @@ echo '<p>@'.$screen_name.' |  <a href='.LOGIN.'/logout.php" title="Click here to
 
 //The user has not logged in
 else {
-echo '<a href="'.LOGIN.'/login_to_twitter.php"><img src="'.LOGIN.'/images/twitterbutton.png" title="Click here to sign in with Twitter!" /></a>';
+echo'
+<html>
+<head>
+	<title>University Stress Analytics</title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <div id="container">
+        <form>
+            <label for="email">Email:</label>
+            <input type="email" id="email" placeholder="Email Address">
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password">
+            <div id="lower">
+                <input type="submit" value="Login">
+            </div><!--/ lower-->
+        </form>
+<br/>
+<br/>
+OR 
+<br/>
+<br/>
+';
+echo '<a href="'.LOGIN.'/login_to_twitter.php"><img src="images/twitterbutton.png" title="Click here to sign in with Twitter!" /></a>';
+
+echo '
+</div>
+</body>
+</html>';
 }
 
 ?>
