a4337b67bc38ecb9f526f1b8ac11045ca9776d7e
nisargmodi <nisarg92modi@gmail.com>
2016-06-14 16:45:38 -0500

    fetching tweets and processing code modified

--- src/main/cron-job/includes/keys.php
+++ src/main/cron-job/includes/keys.php
@@ -3,4 +3,9 @@ define("OAUTH_ACCESS_TOKEN", "811321561-r44VmUPpXeK7ndpcvj9QbKfGWpdtZqPk8EArp8hf
 define("OAUTH_ACCESS_TOKEN_SECRET", "aElMsc9OrT7O3wdqrTHxbjpcNnhBY9YnIq49vnM1eO2a5");
 define("CONSUMER_KEY", "7uBwsMCamrmNAUqB77qUiGjHc");
 define("CONSUMER_SECRET", "PclZ8z3lrRBuEnACrzUo6tGQulyp7wVH6tj5IRUsS5gG6zWQ9A");
+
+define("SERVER_NAME", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+define("DB_NAME", "stressAnalytics");
 ?>
